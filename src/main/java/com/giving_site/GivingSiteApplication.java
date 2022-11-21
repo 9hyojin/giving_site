5011cbd5f03a83a712557d7355e1359c43823c2b
9hyojin <koohyojin1205@gmail.com>
2022-11-21 18:44:45 +0900

    giving site first commit

---
+++ src/main/java/com/giving_site/GivingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.giving_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivingSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GivingSiteApplication.class, args);
+    }
+
+}
