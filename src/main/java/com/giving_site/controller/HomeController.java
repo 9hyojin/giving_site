c36ec64c53b522780e891fbe89ef0a9fdda8bcde
9hyojin <koohyojin1205@gmail.com>
2022-11-22 21:39:31 +0900

    add introduce.html

--- src/main/java/com/giving_site/controller/HomeController.java
+++ src/main/java/com/giving_site/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.giving_site.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,10 @@ public class HomeController {
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/introduce")
+    public String introduce(){
+        return "introduce";
     }
 }
