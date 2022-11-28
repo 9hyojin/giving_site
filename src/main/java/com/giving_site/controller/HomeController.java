6d293dcdc70c88f22466463704b223fa209382c3
9hyojin <koohyojin1205@gmail.com>
2022-11-28 13:55:11 +0900

    add student.html add login.css login.html change whole color

--- src/main/java/com/giving_site/controller/HomeController.java
+++ src/main/java/com/giving_site/controller/HomeController.java
@@ -2,23 +2,38 @@ package com.giving_site.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(){
+
         return "home";
     }
 
     @GetMapping("/introduce")
     public String introduce(){
+
         return "introduce";
     }
 
     @GetMapping("/teacher")
     public String teacher(){
+
         return "teacher";
     }
+
+    @GetMapping("/student")
+    public String student(){
+
+        return "student";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+
+        return "login";
+    }
+
 }
